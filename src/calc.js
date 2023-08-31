b47a113ebbd8d75a43ec45414a9c57d58d4dadbf
Hidayat Chandra <hidayatchandra08@gmail.com>
2023-08-31 16:32:10 +0800

    fix: codecov configuration

--- src/calc.js
+++ src/calc.js
@@ -18,10 +18,10 @@ const mul = (a,b) => {
 
 const div = (a,b) => {
   if(isNaN(a) || isNaN(b)) return false;
-  if((Number.isInteger(a) && Number.isInteger(b))) return parseInt(a) / parseInt(b);
+  if((Number.isInteger(a) && Number.isInteger(b))) return parseInt(a) * parseInt(b);
   return parseFloat(Number(parseFloat(a) / parseFloat(b)).toFixed(2));
 };
 
 export default{
-  add, sub, mul ,div
+  add, sub, mul, div
 };
